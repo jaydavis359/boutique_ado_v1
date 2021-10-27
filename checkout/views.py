a22d42ffd7b01086c2cbd9efc10615336982a257
jaydavis359 <jaydavis_1@hotmail.com>
2021-10-27 11:59:32 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 
+        'pk_test_51JpA2aKoXA0Vs4AUYgSmkXqX99nEAy3qW7erkZ0MY4tq28PSfXJkvODjZX7MPvaHQh9A1QrbWjlieKCtCJdpl5ZT006PWw4vCO',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
