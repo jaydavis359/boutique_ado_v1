f214fec1eb9684e410b89049c5ca6720ed1a09eb
jaydavis359 <jaydavis_1@hotmail.com>
2021-11-02 16:42:32 +0000

    product admin - Finished add product functionality

--- products/views.py
+++ products/views.py
@@ -30,8 +30,8 @@ def all_products(request):
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)    
-
+            products = products.order_by(sortkey)
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -42,11 +42,11 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'{sort}_{direction}'            
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
@@ -71,8 +71,18 @@ def product_detail(request, product_id):
 
 
 def add_product(request):
-    """Add a product to the store """
-    form = ProductForm()
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
